99dcd9f771ce76b5a7df582aa4e9a206e4cbce51
Victor Mary <5032897-VictorMary@users.noreply.gitlab.com>
2020-01-22 14:17:57 +0100

    Opérateurs sur l'écriture

--- testSerial.cc
+++ testSerial.cc
@@ -2,6 +2,25 @@
 
 #include <gtest/gtest.h>
 
+const char* FileName = "FileTest.txt";
+TEST(SerialTests, uint8){
+    
+    double x = 186.95264;
+    {
+    printf("res avant sérialisation : %f\n", x);
+    serial::OBinaryFile serializer(FileName);
+    serializer << x;
+    }
+    
+    double xAfter;
+    serial::IBinaryFile deserializer(FileName);
+    deserializer >> xAfter;
+    printf("res après sérialisaiton : %f\n", xAfter);
+    
+
+
+}
+
 int main(int argc, char* argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
